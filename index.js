5668da7c2af3a52103ace2eb36e9c8264fc96f25
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-01-18 04:30:24 -0800

    some-code

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("Hey, This is the website!")
+const a = 23
+// var a = 6 // declaration
+// redeclaration
+a %= 3
+// Assignment Operater
+alert(a)
+
+var b = 3 + 3
+var c = 5 - 4
+var d = 5 * 4
+var e = 5 / 4
+var f = 5 % 4
